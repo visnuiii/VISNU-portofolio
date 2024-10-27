940bb64df61ac1639af67e5012baa2733aacd218
VISNU RISQI RAMADHANI <149612875+visnuiii@users.noreply.github.com>
2024-10-27 12:16:43 +0700

    VISNU-PORTOFOLIO

---
+++ files/script.js
@@ -0,0 +1,77 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Event Listeners: Handling toggle event
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//  Store color theme for future visits
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //add this
+  }
+}
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+
+var typed = new Typed('.typed', {
+  strings: ["Visnu Risqi"],
+  typeSpeed: 50, // Adjust typing speed (characters per second)
+  backSpeed: 20, // Adjust backspace speed (characters per second)
+  loop: true, // Set to true to loop the animation
+});
+
+window.sr = ScrollReveal(); // Initialize ScrollReveal
+
+sr.reveal('body', {
+  distance: '0px', // Start animation from viewport top
+  origin: 'top',   // Animate elements from the top
+  duration: 800,    // Animation duration in milliseconds
+  easing: 'ease-in-out' // Animation easing function (optional)
+});
